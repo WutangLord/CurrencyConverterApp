2e3a0f16070e58b603edc634e4d5cdbeb17ad3e5
wutanglord <chemaket.chepkwony97@gmail.com>
2025-04-06 22:55:47 +0300

    improve the background UI with shade of violet

--- Desktop/MyCapstoneProject/currency-converter/src/App.jsx
+++ Desktop/MyCapstoneProject/currency-converter/src/App.jsx
@@ -3,7 +3,7 @@ import CurrencyConverter from './components/Currency-Converter';
 
 function App() {
   return (
-    <div className='min-h-screen bg-red-300 flex flex-col items-center justify-center text-black'>
+    <div className='min-h-screen bg-violet-500 flex flex-col items-center justify-center text-black'>
       <div className='container'>
 
       <CurrencyConverter />
