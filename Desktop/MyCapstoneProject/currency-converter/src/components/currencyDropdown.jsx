656c62986f1bf395323255ab58fdd93791b49040
wutanglord <chemaket.chepkwony97@gmail.com>
2025-04-06 21:12:37 +0300

    styling of individual selct buttons for from and start currency exchange

--- Desktop/MyCapstoneProject/currency-converter/src/components/currencyDropdown.jsx
+++ Desktop/MyCapstoneProject/currency-converter/src/components/currencyDropdown.jsx
@@ -8,8 +8,8 @@ const CurrencyDropdown = ({
 }) => {
   return (
     <div>
-      <label htmlFor={title}>{title}</label>
-      <select
+      <label className='block text-lg font-semibold text-red-700 ' htmlFor={title}>{title}</label>
+      <select className='w-full p-2 border-gray-600 rounded-lg shadow-emerald-200 focus:outline-none focus:ring-4 focus:ring-violet-500'
         id={title}
         value={currency}
         onChange={(e) => setCurrency(e.target.value)}
