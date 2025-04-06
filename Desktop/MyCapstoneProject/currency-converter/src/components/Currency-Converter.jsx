dc18c6249e7091151169c08e01931a8f764ccc38
wutanglord <chemaket.chepkwony97@gmail.com>
2025-04-06 12:43:59 +0300

    styling the currency converter component for UI  responsiveness

--- Desktop/MyCapstoneProject/currency-converter/src/components/Currency-Converter.jsx
+++ Desktop/MyCapstoneProject/currency-converter/src/components/Currency-Converter.jsx
@@ -1,6 +1,15 @@
 
 const CurrencyConverter = () => {
-  return  <div>CurrencyConverter</div>;
+  return (
+  <div className="max-w-2xl mx-auto my-11 p-6 bg-gray-100 rounded-3xl shadow-xl ">
+    <h2 className="mb-7 text-3xl font-bold text-green-800">CurrencyConverter</h2>
+    <div>Dropdownbutton</div>
+    <div>
+      <label htmlFor="amount">Amount</label>
+    </div>
+  </div>
+  
+  );
 };
 
 export default CurrencyConverter;
